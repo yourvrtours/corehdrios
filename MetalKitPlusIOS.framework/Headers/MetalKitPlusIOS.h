4bad124dbbade20f499cfc14b80e7e99e92d95ce
zarearia <61406011+zarearia@users.noreply.github.com>
2022-02-04 11:30:04 +0330

    MetalKitPlusIOS added
    consider creating a new repository to add the code of MetalKitPlusIOS, this framework is for simulators, you can develop the device version from the code as well. 
    I can convert both for you in form of an xcframework when the code is functional!

---
+++ MetalKitPlusIOS.framework/Headers/MetalKitPlusIOS.h
@@ -0,0 +1,18 @@
+//
+//  MetalKitPlusIOS.h
+//  MetalKitPlusIOS
+//
+//  Created by Aria Zare on 2/2/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MetalKitPlusIOS.
+FOUNDATION_EXPORT double MetalKitPlusIOSVersionNumber;
+
+//! Project version string for MetalKitPlusIOS.
+FOUNDATION_EXPORT const unsigned char MetalKitPlusIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetalKitPlusIOS/PublicHeader.h>
+
+
